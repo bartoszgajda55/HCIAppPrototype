2bd193a9283e2fc3a771e528e08ad9745e16327e
Bartosz Gajda <bg@bartoszgajda.com>
2017-11-03 14:17:47 +0000

    Search page done (and working!)

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the SearchPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import {IonicPage, NavController, NavParams, PopoverController} from 'ionic-angular';
+import {LogoutComponent} from "../../components/logout/logout";
+import {PostsProvider} from "../../providers/posts/posts";
+import {Post} from "../../models/post.model";
 
 @IonicPage()
 @Component({
@@ -14,12 +10,36 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'search.html',
 })
 export class SearchPage {
+  posts: Post[];
+  searchTerm: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private popoverCtrl: PopoverController,
+              private postsProvider: PostsProvider) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SearchPage');
+  ionViewWillEnter() {
+    this.intializePosts();
   }
 
+  onShowOption(event: MouseEvent): void {
+    let popover = this.popoverCtrl.create(LogoutComponent);
+    popover.present({ev: event});
+  }
+
+  filterPosts(ev: any): void {
+    this.intializePosts();
+
+    let val = ev.target.value;
+    if (val && val.trim() != '') {
+      this.posts = this.posts.filter((item) => {
+        return (item.title.toLocaleLowerCase().indexOf(val.toLowerCase()) > -1);
+      });
+    }
+  }
+
+  private intializePosts(): void {
+    this.posts = this.postsProvider.getPosts();
+  }
 }
