d9d8bfaeec890c32b80972402dec1d057a6c619a
Bartosz Gajda <bg@bartoszgajda.com>
2017-11-04 18:08:50 +0000

    Changed user data. Added basic getter.

--- src/providers/user.data.ts
+++ src/providers/user.data.ts
@@ -1,10 +1,12 @@
-export default {
-  "name": "John Doe",
-  "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
-  "accountsConnected": {
-    "facebook": true,
-    "instagram": false
-  },
-  "email": "jdoe@gmail.com",
-  "password": "password"
-}
+export default [
+  {
+    "name": "John Doe",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
+    "accountsConnected": {
+      "facebook": true,
+      "instagram": false
+    },
+    "email": "jdoe@gmail.com",
+    "password": "password"
+  }
+]
--- src/providers/user/user.ts
+++ src/providers/user/user.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
-import User from '../user.data';
+import Users from '../user.data';
+import {User} from "../../models/user.model";
 
 @Injectable()
 export class UserProvider {
+  user: User = Users[0];
 
   constructor(public http: Http) {
+  }
 
+  getUser(): User {
+    return this.user;
   }
 
 }
