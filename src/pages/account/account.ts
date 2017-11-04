da48509cbd35326e7887eb99b81a9860e6bbb164
Bartosz Gajda <bg@bartoszgajda.com>
2017-11-04 18:52:38 +0000

    Account Page done.

--- src/pages/account/account.ts
+++ src/pages/account/account.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import {IonicPage, NavController, NavParams, PopoverController} from 'ionic-angular';
+import {IonicPage, NavController, NavParams, PopoverController, ToastController} from 'ionic-angular';
 import {LogoutComponent} from "../../components/logout/logout";
+import {User} from "../../models/user.model";
+import {UserProvider} from "../../providers/user/user";
+import {NgForm} from "@angular/forms";
 
 @IonicPage()
 @Component({
@@ -8,14 +11,27 @@ import {LogoutComponent} from "../../components/logout/logout";
   templateUrl: 'account.html',
 })
 export class AccountPage {
+  user: User;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
-              private popoverCtrl: PopoverController) {
+              private popoverCtrl: PopoverController,
+              private userProvider: UserProvider,
+              private toastCtrl: ToastController) {
+    this.user = this.userProvider.getUser();
   }
 
   onShowOption(event: MouseEvent): void {
     let popover = this.popoverCtrl.create(LogoutComponent);
     popover.present({ev: event});
+  }
+
+  onChangePassword(form: NgForm): void {
+    let toast = this.toastCtrl.create({
+      message: "Password Changed!",
+      duration: 2000
+    });
+    toast.present();
+    form.reset();
   }
 }
